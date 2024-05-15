6a9819dc82a6c6590b2caea80daff56b311dec51
Maria-Steliana Cârlan <maya_dlr03@yahoo.com>
2024-05-15 19:27:09 +0300

    salary now can only be updated by respecting budget gap firing is now possible

---
+++ flask_app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+
+import company_func
+
+config = company_func.initialize_config()
+app = Flask(__name__)
+
+@app.route("/")
+def first_func():
+    return {"message": "Hello world!"}
+
+
+@app.route("/home", methods=['PUT', 'POST'])
+def second_func():
+    print(request.method)
+    data = request.data
+    data = data.decode()
+    print(data)
+    return {"message": "Post or put request"}
+
+@app.route("/emps/<emp_id>")
+def get_employees(emp_id):
+    if emp_id:
+        sql_query = f"select * from company.employees where emp_id = {emp_id}"
+        return company_func.read_from_database(sql_query, config)
+
+
+
+if __name__ == '__main__':
+    app.run()
